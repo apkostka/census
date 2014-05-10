79771d0b51a5c5899c24b04be0d69a0c179d6e66
Andrew Kostka <andrew.kostka@vantagelocal.com>
2014-05-09 18:27:20 -0700

    Initialized with google maps stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
+  /* Data points defined as an array of LatLng objects */
+	
+
+	var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+	map = new google.maps.Map(document.getElementById('map_canvas'), {
+	  center: sanFrancisco,
+	  zoom: 13,
+	  mapTypeId: google.maps.MapTypeId.MAP
+	});
+
+	var heatmap = new google.maps.visualization.HeatmapLayer({
+	  data: heatmapData
+	});
+	heatmap.setMap(map);
 
 });
